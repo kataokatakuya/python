11c73eb5053886486f57adf05600c135dc178ab4
kataokatakuya <kataokatakuya>
2020-02-13 00:57:28 +0900

    多項式回帰の実装
    学習データをもとに、学習を繰り返して、パラメータを更新。パラメータを更新する中で、誤差が0.01以下となった時の2次関数を抽出して、グラフにプロ
    ット。

--- math-basics/regression.py
+++ math-basics/regression.py
@@ -23,6 +23,8 @@ plt.show()
 """
 2-2  1次関数として実装
 """
+
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -88,4 +90,65 @@ x = np.linspace(-3, 3, 100)
 plt.plot(train_z, train_y, 'o')
 plt.plot(x, f(x))
 plt.show()
+"""
 
+
+
+"""
+2-4  多項式回帰の実装
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 学習データを読み込む
+train = np.loadtxt('click.csv', delimiter=',', dtype='int', skiprows=1)
+train_x = train[:, 0]
+train_y = train[:, 1]
+
+# 標準化
+mu = train_x.mean()
+sigma = train_x.std()
+def standardize(x):
+    return (x - mu) / sigma
+
+train_z = standardize(train_x)
+
+# 目的関数
+def E(x, y):
+    return 0.5 * np.sum((y - f(x)) ** 2)
+
+# 学習率
+ETA = 1e-3
+
+# 誤差の差分
+diff = 1
+
+# パラメータを初期化
+theta = np.random.rand(3)
+
+# 学習データの行列を作る
+def to_matrix(x):
+    return np.vstack([np.ones(x.shape[0]), x, x ** 2]).T
+
+X = to_matrix(train_z)
+
+# 予測関数
+def f(x):
+    return np.dot(x, theta)
+
+
+# 学習を繰り返す
+error = E(X, train_y)
+while diff > 1e-2:
+    # パラメータを更新
+    theta = theta - ETA * np.dot(f(X) - train_y, X)
+    # 前回の誤差との差分を計算
+    current_error = E(X, train_y)
+    diff = error - current_error
+    error = current_error
+
+# プロット
+x = np.linspace(-3, 3, 100)
+plt.plot(train_z, train_y, 'o')
+plt.plot(x, f(to_matrix(x)))
+plt.show()
