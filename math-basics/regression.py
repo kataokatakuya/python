78d22fcef7e4178da8f60fb96756feb3231ff635
kataokatakuya <kataokatakuya>
2020-02-19 00:24:36 +0900

    平均二乗誤差
    学習データをもとに学習を繰り返したときの誤差の収束をグラフにプロットする

--- math-basics/regression.py
+++ math-basics/regression.py
@@ -97,6 +97,8 @@ plt.show()
 """
 2-4  多項式回帰の実装
 """
+
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -152,3 +154,72 @@ x = np.linspace(-3, 3, 100)
 plt.plot(train_z, train_y, 'o')
 plt.plot(x, f(to_matrix(x)))
 plt.show()
+"""
+
+
+
+"""
+2-4の続き  平均二乗誤差
+繰り返し回数を横軸、平均二乗誤差を縦軸にしてプロット
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# 学習データを読み込む
+train = np.loadtxt('click.csv', delimiter=',', dtype='int', skiprows=1)
+train_x = train[:, 0]
+train_y = train[:, 1]
+
+# 標準化
+mu = train_x.mean()
+sigma = train_x.std()
+def standardize(x):
+    return (x - mu) / sigma
+
+# 学習データの行列を作る
+def to_matrix(x):
+    return np.vstack([np.ones(x.shape[0]), x, x ** 2]).T
+
+# 予測関数
+def f(x):
+    return np.dot(x, theta)
+
+# 平均二乗誤差
+def MSE(x, y):
+    return (1 / x.shape[0]) * np.sum((y - f(x)) ** 2)
+
+# z-score化(学習データの平均を0、分散を1に変換する)したパラメータ
+train_z = standardize(train_x)
+
+#学習データの行列
+X = to_matrix(train_z)
+
+# パラメータをランダムに初期化
+theta = np.random.rand(3)
+
+# 平均二乗誤差の履歴
+errors = []
+
+# 学習率
+ETA = 1e-3
+
+# 誤差の差分
+diff = 1
+
+# 学習を繰り返す
+errors.append(MSE(X, train_y))
+
+while diff > 1e-2:
+    theta = theta - ETA * np.dot(f(X) - train_y, X)
+    errors.append(MSE(X, train_y))
+    diff = errors[-2] - errors[-1]
+
+# 誤差をプロット
+x = np.arange(len(errors))
+
+plt.plot(x, errors)
+plt.show()
+
+
+
